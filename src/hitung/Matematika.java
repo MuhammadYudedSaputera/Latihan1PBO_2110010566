827556d597b65d6b288ed9c909075a2719b03cc3
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-12 20:02:11 +0800

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -14,4 +14,19 @@ public class Matematika {
     public double setPenjumlahan (){
         return bil1 + bil2;
     }
+    
+    //membuat method setpengurangan
+    public double setPengurangan (){
+        return bil1 - bil2;
+    }
+    
+    //membuat method setperkalian
+    public double setPerkalian (){
+        return bil1 * bil2;
+    }
+    
+    //membuat method setpembagian
+    public double setPembagian(){
+        return bil1 / bil2;
+    }
 }
