e6e4647cdf9c8c4bf01ddd4c10113e52ff1eac1a
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-12 20:17:13 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author ASUS
- */
 public class MatematikaBeraksi {
-    
+    public static void main (String[] args) {
+        
+        Matematika yuded = new Matematika (2, 3);
+        
+        System.out.println ("Hasil penjumlahan: "+ yuded.setPenjumlahan ());
+        System.out.println ("Hasil pengurangan: "+ yuded.setPengurangan ());
+        System.out.println ("Hasil perkalian: "+ yuded.setPerkalian ());
+        System.out.println ("Hasil pembagian: "+ yuded.setPembagian ());
+    }
 }
